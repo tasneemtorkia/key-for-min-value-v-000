b7f4f5c626db1e7fcecec3b22a03f9baee7e6685
tasneem torkia <tasneemtorkia@hotmail.co.uk>
2018-05-03 07:01:38 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,9 +3,9 @@
 
 def key_for_min_value(name_hash)
   lk = nil 
-  lv = Float::INFINITY 
+  lv = nil 
   name_hash.each do |key, value|
-  if value < lv 
+  if lv == nil || value < lv 
   lv = value 
   lk = key
 end 
